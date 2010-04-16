de543f863ccd14695b66bbcee945cfbb706f0c42
SATO Yozo <yoozoosato@gmail.com>
2010-04-16 10:21:25 +0900

    those wiki's are already closed.

--- pukiwiki.el
+++ pukiwiki.el
@@ -1,12 +1,15 @@
 ;; 
 ;; pukiwiki-mode
-;;
+;; http://www.bookshelf.jp/pukiwiki/pukiwiki.php?%A5%A2%A5%A4%A5%C7%A5%A2%BD%B8%2Fpukiwiki-mode
 (load-library "pukiwiki-mode")
 (setq pukiwiki-site-list
       '(
+	;; Genesis Lightning Talks
 	("lt" "http://wiki.somethingnew2.com/lt/index.php" nil euc-jp-dos)
-	("AppFuse" "http://wiki.somethingnew2.com/AppFuse/index.php" nil euc-jp-dos)
-	("insight" "http://wiki.somethingnew2.com/insightWiki/index.php" nil euc-jp-dos)
 	))
 
+;; sava post data on local disc ('~/pukiwiki' is default directory)
 (setq pukiwiki-save-post-data t)
+
+;; use ediff when viewing changeset
+(setq pukiwiki-diff-using-ediff t)
