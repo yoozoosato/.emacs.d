80053db62541d6bb8fd76b37cb1e96b2e6e8972e
SATO Yozo <yoozoosato@gmail.com>
2010-04-20 20:58:43 +0900

    color-theme is a display library.

--- display-settings.el
+++ display-settings.el
@@ -1,3 +1,6 @@
+;;
+;; emacs settings
+;;
 (setq inhibit-startup-message t)
 (setq visible-bell t)
 (setq blink-matching-paren t)
@@ -6,3 +9,12 @@
 (tool-bar-mode 0)
 (show-paren-mode t)
 (transient-mark-mode 1)
+
+;; 
+;; color-theme
+;;
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-clarity)
+
+
