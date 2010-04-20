9eb0d6c3a5903d8469436fdbc305ad7ac945381d
SATO Yozo <yoozoosato@gmail.com>
2010-04-20 20:18:00 +0900

    create new elisp setting file

---
+++ jde.el
@@ -0,0 +1,10 @@
+;; 
+;; jde
+;; 
+(setenv "JAVA_HOME" "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home")
+(require 'jde)
+
+
+;; name, email address. 
+(setq user-full-name "SATO Yozo")
+(setq user-mail-address "yoozoosato@gmail.com")
