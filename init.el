06b2840c03f1cd843fa23081b8d0e4d36293cb33
SATO Yozo <yoozoosato@gmail.com>
2010-04-20 21:17:00 +0900

    separated from .emacs.el.

---
+++ init.el
@@ -0,0 +1,20 @@
+;; 
+;; load files
+;;
+;; mona
+(load "~/.emacs.d/mona.el")
+;; jde
+(load "~/.emacs.d/jde.el")
+;; pukiwiki-mode
+(load "~/.emacs.d/pukiwiki.el")
+;; twittering-mode
+(load "~/.emacs.d/twittering-mode.el")
+;; howm
+(load "~/.emacs.d/howm.el")
+;; anything
+(load "~/.emacs.d/anything.el")
+
+;; key-bind
+(load "~/.emacs.d/keybind.el")
+(load "~/.emacs.d/display-settings.el")
+(load "~/.emacs.d/calendar-settings.el")
