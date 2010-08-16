153a15660ff27ab41fbd7190c2b538eb2895a3db
SATO Yozo <yoozoosato@gmail.com>
2010-08-16 15:34:38 +0900

    try to use "git submodule add"

--- inits/20_ido.el
+++ inits/20_ido.el
@@ -1,3 +1,13 @@
 ;; Interactively Do Things (highly recommended, but not strictly required)
-(require 'ido)
-(ido-mode t)
+(cond
+ ((string-match "apple-darwin" system-configuration)
+  ;; carbon-emacsではなぜか上手く動作しない
+  )
+ ((string-match "linux" system-configuration)
+  ;; Ubuntuのemacs23なら問題無し
+  (require 'ido)
+  (ido-mode t)
+  )
+ ((string-match "mingw" system-configuration)
+  )
+ )
