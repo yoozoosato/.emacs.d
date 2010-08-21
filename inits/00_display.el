cac0ecc2e70847d7346bc0b26bb64848f3c307a2
SATO Yozo <yoozoosato@gmail.com>
2010-08-22 03:21:54 +0900

    fixed conflict.

--- inits/00_display.el
+++ inits/00_display.el
@@ -8,31 +8,6 @@
 ;; when warn, screen flashes
 (setq visible-bell t)
 
-<<<<<<< HEAD
-;; parens
-(show-paren-mode t)
-(setq blink-matching-paren t)
-
-(line-number-mode t)
-(column-number-mode t)
-
-;; 
-(tool-bar-mode nil)
-(scroll-bar-mode nil)
-
-(transient-mark-mode 1)
-
-;; default frame. size and position.
-(setq initial-frame-alist
-      (append (list
-               '(width . 80)  ;; ウィンドウ幅
-               '(height . 46) ;; ウィンドウ高さ
-               '(top . 30)    ;; 表示位置
-               '(left . 20)  ;; 表示位置
-               )
-              initial-frame-alist))
-(setq default-frame-alist initial-frame-alist)
-=======
 ;; highlight another paren when cursor hits one
 (show-paren-mode t)
 (setq blink-matching-paren t)
@@ -44,7 +19,9 @@
 ;; hide toolbar and scroll bar
 (tool-bar-mode 0)
 (scroll-bar-mode nil)
->>>>>>> 8cc8396b5a6ca11c3b090d5376f28496e9e8288d
+
+;; forgot
+(transient-mark-mode 1)
 
 ;; default frame settings
 (cond
