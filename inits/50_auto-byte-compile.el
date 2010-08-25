9f7b39267c6fda176deb0ad3aae0a1166727f64f
SATO Yozo <yoozoosato@gmail.com>
2010-08-25 14:49:22 +0900

    色々設定

--- inits/50_auto-byte-compile.el
+++
@@ -1,5 +0,0 @@
-;;
-;; auto-async-byte-compile
-;; http://www.emacswiki.org/emacs/AutoAsyncByteCompile
-(require 'auto-async-byte-compile)
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
