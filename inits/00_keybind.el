c77b8441fcf31f14b14deea060763b9f91711a58
SATO Yozo <yoozoosato@gmail.com>
2010-08-25 15:18:59 +0900

    when ido-mode is enabled, \C-xk didn't work well without global-map config.

--- inits/00_keybind.el
+++ inits/00_keybind.el
@@ -2,6 +2,7 @@
 ;; key-bind
 ;;
 (define-key global-map "\C-h" 'delete-backward-char)
+(define-key global-map "\C-xk" 'kill-this-buffer)
 
 ;; input non ascii characters in mini buffer while incremental-search
 (define-key isearch-mode-map "\C-k" 'isearch-edit-string)
