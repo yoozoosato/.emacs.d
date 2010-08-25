bc7dace95f6c62f2a2fe31119fe9bd38181393a9
SATO Yozo <yoozoosato@gmail.com>
2010-08-25 16:15:06 +0900

    to read lots of source codes much faster.

---
+++ inits/50_gtags.el
@@ -0,0 +1,11 @@
+;;
+;; GNU GLOBAL(gtags)
+;; http://www.emacswiki.org/emacs/GnuGlobal
+(autoload 'gtags-mode "gtags" "" t)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\C-t" 'gtags-pop-stack)
+         ))
