ba539ea9cb4ae283710e8450955de577c17fa3a8
SATO Yozo <yoozoosato@gmail.com>
2010-08-26 16:53:53 +0900

    やっぱ止めた、という操作を指が覚えているので。。。

--- inits/30_twit.el
+++ inits/30_twit.el
@@ -7,3 +7,8 @@
 ;  (setq twittering-password "")
   (twittering-icon-mode t)
 )
+
+;; same key-map as mew
+(when twittering-edit-mode-map
+  (let ((km twittering-edit-mode-map))
+    (define-key km (kbd "C-c C-q") 'twittering-edit-cancel-status)))
