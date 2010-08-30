8255e52ddf8b415d2f4367fb8b5275bc08372e3b
SATO Yozo <yoozoosato@gmail.com>
2010-08-30 19:53:49 +0900

    set Think Park Tower's lat & lon.

--- inits/30_calendar.el
+++ inits/30_calendar.el
@@ -1,8 +1,8 @@
 ;;
 ;; for calendar & diary
 ;; latitude and longitude are required for moon phases, sunrise and sunset
-(setq calendar-latitude 35.42)
-(setq calendar-longitude 139.46)
+(setq calendar-latitude 35.618564)
+(setq calendar-longitude 139.727667)
 
 ;; 9 hours * 60 minuts
 (setq calendar-time-zone +540)
@@ -11,4 +11,3 @@
 (setq calendar-location-name "Tokyo, JPN")
 (setq diary-mail-addr "yoozoosato@gmail.com")
 (setq diary-file "~/Dropbox/Documents/diary")
-
