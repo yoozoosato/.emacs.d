63188025a5074032471d5fd409e47906a63004b4
SATO Yozo <yoozoosato@gmail.com>
2010-08-31 10:50:15 +0900

    viewer-mode basic settings. view-mode files are read only, mode line color is different from writable files.

---
+++ inits/20_view-mode.el
@@ -0,0 +1,11 @@
+;;
+;; view-mode
+;; http://www.emacswiki.org/emacs/ViewMode
+;; Emacs Technic Bible p.216
+
+(setq view-mode t)
+(setq view-read-only t)
+(require 'viewer)
+(setq viewer-modeline-color-unwritable "tomato")
+(setq viewer-modeline-color-view "orange")
+(viewer-change-modeline-color-setup)
