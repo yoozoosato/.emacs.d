e51947a3a614f3b1f829b8f696d7b58359c24adf
SATO Yozo <yoozoosato@gmail.com>
2010-09-15 17:27:34 +0900

    changed to use w3m. Ubuntu emacs 23 requires snapshot package.

--- inits/30_w3m.el
+++ inits/30_w3m.el
@@ -1,6 +1,6 @@
 ;; 
 ;; emacs-w3m
 ;; http://emacs-w3m.namazu.org/
-;; (require 'w3m-load)
-;; (autoload 'w3m "w3m" "Interface for w3m on Emacs." t)
+(require 'w3m-load)
+(autoload 'w3m "w3m" "Interface for w3m on Emacs." t)
 
