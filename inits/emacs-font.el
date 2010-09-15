26693aa4b9f053bc9fc7afc06674feeb1518ee29
SATO Yozo <yoozoosato@gmail.com>
2010-09-15 18:00:05 +0900

    this file will not be loaded.

---
+++ inits/emacs-font.el
@@ -0,0 +1,10 @@
+;;
+;; font setting for Emacs on Linux
+;;
+
+(set-default-font "Inconsolata-11")
+(set-face-font 'variable-pitch "Inconsolata-11")
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  '("Takaoゴシック" . "unicode-bmp")
+)
