ab1e02211096a55a7625ed80c1bcbd1dd541de98
SATO Yozo <yoozoosato@gmail.com>
2010-09-22 12:25:34 +0900

    add ignore settings.

--- inits/50_yasnippet.el
+++ inits/50_yasnippet.el
@@ -1,4 +1,18 @@
 ;; yasnippet
+;;
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/lisp/yasnippet-0.6.1c/snippets/")
+
+;; default ix x-popup
+(require 'dropdown-list)
+(setq yas/prompt-functions '(yas/dropdown-prompt))
+
+;; for literal and comment
+(setq yas/buffer-local-condition
+      '(or (not (or (string= "font-lock-comment-face"
+                             (get-char-property (point) 'face))
+                    (string= "font-lock-string-face"
+                             (get-char-property (point) 'face))))
+           '(require-snippet-condition . force-in-comment)))
+
