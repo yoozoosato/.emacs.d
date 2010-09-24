1ef577e92ea8a19f0940ed4e23e0e0135ec23ad6
SATO Yozo <yoozoosato@gmail.com>
2010-09-24 12:24:45 +0900

    for org-mode

---
+++ inits/30_org.el
@@ -0,0 +1,6 @@
+;; org-mode
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
