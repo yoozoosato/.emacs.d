de449231119a98f831f9cc2f22d59262b116ce87
SATO Yozo <yoozoosato@gmail.com>
2010-11-26 14:19:58 +0900

    add install expression.

--- inits/51_usage-memo.el
+++ inits/51_usage-memo.el
@@ -1,6 +1,6 @@
 ;;
 ;; usage-memo
-;; http://www.emacswiki.org/emacs/UsageMemo
+;; (install-elisp-from-emacswiki "http://www.emacswiki.org/emacs/usage-memo.el")
 (require 'usage-memo)
 (setq umemo-base-directory "~/Dropbox/Documents/umemo")
 (umemo-initialize)
