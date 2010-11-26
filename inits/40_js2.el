e7d2f8c2dfeb70fb681faed3b0f865646b6a720a
SATO Yozo <yoozoosato@gmail.com>
2010-11-26 14:19:30 +0900

    add install expression.

--- inits/40_js2.el
+++ inits/40_js2.el
@@ -1,5 +1,5 @@
 ;;
 ;; js2-mode
-;; 
+;; (install-elisp "http://js2-mode.googlecode.com/files/js2-20090723b.el")
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
