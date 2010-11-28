884ee2e71d7775e8200ca6194736b19ecfb31e43
SATO Yozo <yoozoosato@gmail.com>
2010-11-28 16:03:14 +0900

    for sddiv emacs study

--- inits/40_ruby.el
+++ inits/40_ruby.el
@@ -6,13 +6,15 @@
 ;(require 'rvm)
 ;(rvm-use-default)
 
-;; use rsense
-;; http://cx4a.org/software/rsense/index.html
-(setq rsense-home (expand-file-name "~/bin/rsense-0.3"))
-(add-to-list 'load-path (concat rsense-home "/etc"))
-(require 'rsense)
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-c .") 'ac-complete-rsense)))
-(setq rsense-rurema-home "~/src/rurema")
+
+
+;; ;; use rsense
+;; ;; http://cx4a.org/software/rsense/index.html
+;; (setq rsense-home (expand-file-name "~/bin/rsense-0.3"))
+;; (add-to-list 'load-path (concat rsense-home "/etc"))
+;; (require 'rsense)
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-c .") 'ac-complete-rsense)))
+;; (setq rsense-rurema-home "~/src/rurema")
 
