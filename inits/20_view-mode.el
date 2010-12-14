e29669251d4694057a882c9d9ed74e029245a04e
SATO Yozo <yoozoosato@gmail.com>
2010-12-14 12:03:35 +0900

    move key-chord-binding to keybind.el.

--- inits/20_view-mode.el
+++ inits/20_view-mode.el
@@ -3,12 +3,9 @@
 ;; (install-elisp-from-emacswiki "http://www.emacswiki.org/emacs/viewer.el")
 ;; Emacs Technic Bible p.216
 
-(setq view-mode t)
-(setq view-read-only t)
 (require 'viewer)
 (setq viewer-modeline-color-unwritable "tomato")
 (setq viewer-modeline-color-view "orange")
 (viewer-change-modeline-color-setup)
-
-;; key-bind using key-chord
-(key-chord-define-global "jk" 'view-mode)
+(setq view-mode t)
+(setq view-read-only t)
