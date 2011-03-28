22408b9b44429cd7f6a760edc936e8e7b04c31eb
SATO Yozo <yoozoosato@gmail.com>
2011-03-28 16:49:45 +0900

    emacs technique bible 10.1

---
+++ inits/40_text-adjust.el
@@ -0,0 +1,7 @@
+;;
+;; text-adjst
+;;
+;; (install-elisp "http://taiyaki.org/elisp/mell/src/mell.el")
+;; (install-elisp "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
+
+(require 'text-adjust)
