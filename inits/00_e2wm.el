cfc0f7c52409553ee104f620e737160e4c18f475
SATO Yozo <yoozoosato@gmail.com>
2011-08-23 10:26:01 +0900

    switch comment (history, clock)

--- inits/00_e2wm.el
+++ inits/00_e2wm.el
@@ -1,10 +1,10 @@
 ;; window layout manager.
-;; 
+;;
 ;; (auto-install-from-url "https://github.com/kiwanami/emacs-window-layout/raw/master/window-layout.el")
 ;; (auto-install-from-url "https://github.com/kiwanami/emacs-window-manager/raw/master/e2wm.el")
 ;; (auto-install-from-url "https://github.com/kiwanami/emacs-window-manager/raw/master/e2wm-config.el")
 (require 'e2wm)
-(e2wm:add-keymap 
+(e2wm:add-keymap
  e2wm:pst-minor-mode-keymap
  '(("<M-left>" . e2wm:dp-code) ; codeへ変更
    ("<M-right>"  . e2wm:dp-two)  ; twoへ変更
@@ -19,17 +19,18 @@
 ;; ;; ;; for 1024x768
 ;; (setq e2wm:c-code-recipe
 ;;       '(| (:left-max-size 35)
-;; 	  (- (:upper-size-ratio 0.7)
-;; 	     (- (:upper-size-ratio 0.6)
-;; 		files imenu)
-;; 	     history)
-;; 	  (- (:upper-size-ratio 0.7)
-;; 	     main sub)))
+;;    (- (:upper-size-ratio 0.7)
+;;       (- (:upper-size-ratio 0.6)
+;;      files imenu)
+;;       history)
+;;    (- (:upper-size-ratio 0.7)
+;;       main sub)))
 
 (setq e2wm:c-code-winfo
   '((:name main)
     (:name files :plugin files)
-    (:name history :plugin history-list)
+;;     (:name history :plugin history-list)
+    (:name history :plugin clock)
     (:name sub :buffer "*info*" :default-hide t)
     (:name imenu :plugin imenu :default-hide nil))
   )
