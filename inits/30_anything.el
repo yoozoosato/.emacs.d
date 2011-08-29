3fe9947f327b2267e724980d0d570e1d61867e63
SATO Yozo <yoozoosato@gmail.com>
2011-08-29 15:27:38 +0900

    add sources.

--- inits/30_anything.el
+++ inits/30_anything.el
@@ -1,13 +1,28 @@
-;; 
+;;
 ;; anything
 ;; (auto-install-batch anything)
 (require 'anything)
 (require 'anything-config)
 (require 'anything-migemo)
 (global-set-key "\C-x;" 'anything)
-(global-set-key "\C-x:" 'anything-migemo)
+(global-set-key "\C-x:" 'anything-resume)
 (anything-iswitchb-setup)
 (define-key anything-map "\C-p" 'anything-previous-line)
 (define-key anything-map "\C-n" 'anything-next-line)
-(define-key anything-map "\C-v" 'anything-next-page)
-(define-key anything-map "\M-v" 'anything-previous-page)
+(define-key anything-map "\C-\M-v" 'anything-next-page)
+(define-key anything-map "\C-\M-y" 'anything-previous-page)
+
+;; http://d.hatena.ne.jp/tomoya/20090423/1240456834
+;; http://www24.atwiki.jp/anything/m/pages/13.html?guid=on#id_b64e0dd3
+(setq anything-sources
+      '(anything-c-source-buffers+
+        anything-c-source-colors
+        anything-c-source-recentf
+;;         anything-c-source-man-pages
+        anything-c-source-emacs-commands
+        anything-c-source-emacs-functions
+        anything-c-source-files-in-current-dir
+;;         anything-c-source-mac-spotlight
+;;         anything-c-source-google-suggest
+        anything-c-source-kill-ring
+        ))
