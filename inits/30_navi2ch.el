b9948077457ffac03baa19a87bd0cc7a5a0021d7
SATO Yozo <yoozoosato@gmail.com>
2011-09-02 16:21:23 +0900

    add how to install comment.

---
+++ inits/30_navi2ch.el
@@ -0,0 +1,13 @@
+;;
+;; navi2ch 
+;; 2ch viewer for Emacsen
+;; 
+;; how to install navi2ch
+;; cd ~/.emacs.d
+;; git submodule add git://navi2ch.git.sourceforge.net/gitroot/navi2ch/navi2ch \
+;;   ./lisp/navi2ch
+;; cd ./lisp/navi2ch
+;; ./configure
+;; make
+(autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
+(setq navi2ch-icon-directory "~/.emacs.d/lisp/navi2ch/icons")
