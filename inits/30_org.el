4a18faad2cb187eabfddae5d8bfa1622718c984b
SATO Yozo <yoozoosato@gmail.com>
2011-09-02 14:41:04 +0900

    add TODO settings from Emacs Technic Bible.

--- inits/30_org.el
+++ inits/30_org.el
@@ -1,3 +1,6 @@
 ;; org-mode
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(setq org-use-fast-todo-selection t)
+(setq org-todo-keywords
+	  '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")))
