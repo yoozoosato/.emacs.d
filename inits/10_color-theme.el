9b7b4fd204e170919e8e939254d171516cdffdc7
SATO Yozo <yoozoosato@gmail.com>
2011-09-02 17:54:17 +0900

    clarity is cool but tangotango is much better for me.

--- inits/10_color-theme.el
+++ inits/10_color-theme.el
@@ -2,6 +2,7 @@
 ;; color-theme
 ;; http://www.emacswiki.org/emacs/ColorTheme
 (require 'color-theme)
+(require 'color-theme-tangotango)
 ;; Carbon Emacsの場合、initializeが必要。Ubuntu 10.04のEmacs23では必要ない
 (cond
  ((string-match "apple-darwin" system-configuration)
@@ -16,5 +17,5 @@
 ;必要ならここにWindows用の設定を書く
 ; )
 )
-;; (color-theme-tangotango)
-(color-theme-clarity)
+(color-theme-tangotango)
+;; (color-theme-clarity)
