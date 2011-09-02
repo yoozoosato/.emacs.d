b2e7f7f422c2b8305762fad1e9f3662595f097a4
SATO Yozo <yoozoosato@gmail.com>
2011-09-02 18:54:38 +0900

    add path to avoid error on Emacs 23 on Mac OS.

--- inits/30_migemo.el
+++ inits/30_migemo.el
@@ -2,6 +2,7 @@
 ;; migemo
 ;;
 (load "migemo")
+(setq exec-path (cons "/usr/local/bin" exec-path))
 (setq migemo-use-pattern-alist t)
 (setq migemo-use-frequent-pattern-alist t)
 (setq migemo-command "migemo"
