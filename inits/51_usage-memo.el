d449f7d071ad2a88445fc5cbffacacabb8b0a765
SATO Yozo <yoozoosato@gmail.com>
2011-10-11 13:59:26 +0900

    change install function

--- inits/51_usage-memo.el
+++ inits/51_usage-memo.el
@@ -1,6 +1,6 @@
 ;;
 ;; usage-memo
-;; (install-elisp-from-emacswiki "http://www.emacswiki.org/emacs/usage-memo.el")
+;; (auto-install-from-emacswiki "http://www.emacswiki.org/emacs/usage-memo.el")
 (require 'usage-memo)
 (setq umemo-base-directory "~/Dropbox/Documents/umemo")
 (umemo-initialize)
