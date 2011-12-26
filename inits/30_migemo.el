d905f9fc18d54b0f8abd2f78272241712d349670
SATO Yozo <yoozoosato@gmail.com>
2011-12-26 15:56:01 +0900

    migemo only works with ruby 1.8.x. switched to cmigemo.

--- inits/30_migemo.el
+++ inits/30_migemo.el
@@ -1,9 +1,22 @@
 ;;
 ;; migemo
 ;;
-(load "migemo")
-(setq migemo-use-pattern-alist t)
-(setq migemo-use-frequent-pattern-alist t)
-(setq migemo-command "migemo"
-      migemo-options '("-t" "emacs"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  old version 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (load "migemo")
+;; (setq migemo-use-pattern-alist t)
+;; (setq migemo-use-frequent-pattern-alist t)
+;; (setq migemo-command "cmigemo"
+;;       migemo-options '("-t" "emacs"))
+
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
