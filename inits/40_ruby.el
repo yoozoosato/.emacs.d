61e3be2b76a304d42fbeeed8fcd873b4182a05ea
SATO Yozo <yoozoosato@gmail.com>
2012-03-16 15:03:37 +0900

    auto-complete とか使えるように設定してみた
    まだ不完全かもしれないので、これからつかながら直していく

--- inits/40_ruby.el
+++ inits/40_ruby.el
@@ -39,35 +39,35 @@
 ;; http://www.cx4a.org/pub/auto-complete-ruby.el
 ;;(auto-install-from-url "http://www.cx4a.org/pub/auto-complete-ruby.el")
 
-;; (require 'auto-complete)
-;; (require 'rcodetools)
+(require 'auto-complete)
+(require 'rcodetools)
 
-;; (defvar ac-ruby-sources
-;;   '(ac-source-rcodetools))
+(defvar ac-ruby-sources
+  '(ac-source-rcodetools))
 
-;; (defvar ac-source-rcodetools
-;;   '((init . (lambda ()
-;;               (condition-case x
-;;                   (save-excursion
-;;                     (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles"))
-;;                 (error) (setq rct-method-completion-table nil))))
-;;     (candidates . (lambda ()
-;;                     (all-completions
-;;                      ac-prefix
-;;                      (mapcar
-;;                       (lambda (completion)
-;;                         (replace-regexp-in-string "\t.*$" "" (car completion)))
-;;                       rct-method-completion-table))))))
+(defvar ac-source-rcodetools
+  '((init . (lambda ()
+              (condition-case x
+                  (save-excursion
+                    (rct-exec-and-eval rct-complete-command-name "--completion-emacs-icicles"))
+                (error) (setq rct-method-completion-table nil))))
+    (candidates . (lambda ()
+                    (all-completions
+                     ac-prefix
+                     (mapcar
+                      (lambda (completion)
+                        (replace-regexp-in-string "\t.*$" "" (car completion)))
+                      rct-method-completion-table))))))
 
-;; (defun ac-ruby-setup ()
-;;   ;(setq ac-sources (append ac-sources ac-ruby-sources))
-;;   (setq ac-omni-completion-sources (list (cons "\\." ac-ruby-sources)
-;;                                          (cons "::" ac-ruby-sources))))
+(defun ac-ruby-setup ()
+  ;(setq ac-sources (append ac-sources ac-ruby-sources))
+  (setq ac-omni-completion-sources (list (cons "\\." ac-ruby-sources)
+                                         (cons "::" ac-ruby-sources))))
 
-;; (defun ac-ruby-init ()
-;;   (add-hook 'ruby-mode-hook 'ac-ruby-setup))
+(defun ac-ruby-init ()
+  (add-hook 'ruby-mode-hook 'ac-ruby-setup))
   
-;; (provide 'auto-complete-ruby)
+(provide 'auto-complete-ruby)
 
 
 
