2fa1814e12ce700f9c057ffdd81d779fccc7e09c
SATO Yozo <yoozoosato@gmail.com>
2012-06-04 11:41:18 +0900

    anything-sources は非推奨らしい
    http://d.hatena.ne.jp/rubikitch/20100718/anything

--- inits/30_anything.el
+++ inits/30_anything.el
@@ -4,26 +4,12 @@
 (require 'anything-startup)
 (global-set-key "\C-x;" 'anything)
 (global-set-key "\C-x:" 'anything-resume)
+
 ;(anything-iswitchb-setup)
 (define-key anything-map "\C-p" 'anything-previous-line)
 (define-key anything-map "\C-n" 'anything-next-line)
 (define-key anything-map "\C-\M-v" 'anything-next-page)
 (define-key anything-map "\C-\M-y" 'anything-previous-page)
-
-;; http://d.hatena.ne.jp/tomoya/20090423/1240456834
-;; http://www24.atwiki.jp/anything/m/pages/13.html?guid=on#id_b64e0dd3
-(setq anything-sources
-      '(anything-c-source-buffers+
-        anything-c-source-recentf
-        anything-c-source-kill-ring
-        anything-c-source-files-in-current-dir
-;;         anything-c-source-mac-spotlight
-;;         anything-c-source-google-suggest
-;;         anything-c-source-emacs-commands
-;;         anything-c-source-emacs-functions
-;;         anything-c-source-colors
-;;         anything-c-source-man-pages
-        ))
 
 ;; tuning
 ;; (setq anything-candidate-number-limit 100)
