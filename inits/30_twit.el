70159f51b8a28e7c747fdf4dff153dd94eaf5d6e
SATO Yozo <Yozo.Sato@gamepot.co.jp>
2012-09-06 19:38:39 +0900

    最新版に合わせて修正
    認証をGPGで端末に保存するようにした。

--- inits/30_twit.el
+++ inits/30_twit.el
@@ -2,13 +2,29 @@
 ;; twittering-mode
 ;; http://github.com/hayamiz/twittering-mode/
 (when (require 'twittering-mode nil t)
-  (setq twittering-auth-method 'xauth)
-;  (setq twittering-username "yoozoosato")
-;  (setq twittering-password "")
+  (setq twittering-use-master-password t)
+  (setq twittering-private-info-file "~/.emacs.d/twittering-mode.gpg")
   (twittering-icon-mode t)
+
+  ;; use bit.ly for url shorten service
+  (setq twittering-tinyurl-service 'bit.ly)
+  (setq twittering-bitly-login "yoozoosato")
+  (setq twittering-bitly-api-key "R_78b4299f8b0a4aba927917b468f36f6f")
 )
 
 ;; same key-map as mew
 (when twittering-edit-mode-map
   (let ((km twittering-edit-mode-map))
     (define-key km (kbd "C-c C-q") 'twittering-edit-cancel-status)))
+
+;; http://fukuyama.co/twittering-mode
+;; o で次のURLをブラウザでオープン
+(add-hook 'twittering-mode-hook
+          (lambda ()
+            (local-set-key (kbd "o")
+               (lambda ()
+                 (interactive)
+                 (twittering-goto-next-uri)
+                 (execute-kbd-macro (kbd "C-m"))
+                 ))))
+
