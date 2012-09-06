95bcbe2d45ca15e241a303b1f9308ca7b8a49c85
SATO Yozo <yoozoosato@gmail.com>
2012-09-07 01:01:36 +0900

    'text-translator-vars.el' is required to compile 'text-translator.el'

--- inits/30_translator.el
+++ inits/30_translator.el
@@ -1,6 +1,6 @@
 ;;
-;; (auto-install-from-emacswiki "text-translator.el")
 ;; (auto-install-from-emacswiki "text-translator-vars.el")
+;; (auto-install-from-emacswiki "text-translator.el")
 ;; (auto-install-from-emacswiki "text-translator-load.el")
 (require 'text-translator)
 (setq text-translator-auto-selection-func
