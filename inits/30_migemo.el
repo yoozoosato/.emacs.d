7bb2987ed6255e0a6dcc16c36b170f9917479a6f
SATO Yozo <yoozoosato@gmail.com>
2012-09-08 22:03:52 +0900

    Linux用にcmigemoの設定を追加
    Ubuntu の場合は普通に apt-get install cmigemo で cmigemo をインストールするの良いようだ

--- inits/30_migemo.el
+++ inits/30_migemo.el
@@ -1,20 +1,26 @@
 ;;
 ;; migemo
 ;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  old version 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (load "migemo")
-;; (setq migemo-use-pattern-alist t)
-;; (setq migemo-use-frequent-pattern-alist t)
-;; (setq migemo-command "cmigemo"
-;;       migemo-options '("-t" "emacs"))
-
 (require 'migemo)
-(setq migemo-command "cmigemo")
+;; when using Cocoa Emacs, use cmigemo.
+(cond
+ ;; for Mac
+ ((string-match "apple-darwin" system-configuration)
+  (setq migemo-command "cmigemo")
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+  )
+ ;; for Ubuntu Linux
+ ;; migemo was installed with command 'sudo apt-get install migemo'
+ ((string-match "linux" system-configuration)
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs" "-i" "\a"))
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
+  )
+ ;; for Windows
+ ((string-match "mingw" system-configuration)
+  )
+ )
 (setq migemo-options '("-q" "--emacs"))
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
 (setq migemo-coding-system 'utf-8-unix)
