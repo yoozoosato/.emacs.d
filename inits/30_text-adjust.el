eab4fd574d3cb392852afd032011bab651d69ae8
SATO Yozo <Yozo.Sato@gamepot.co.jp>
2012-09-10 16:05:14 +0900

    mew-draft-mode でもできるようにしたいが、 auto-fill との相性が悪い。
    解決案
    (1) auto-fill と text-adjust-space-buffer を一つの関数呼び出しで実行できるようにして、それを hook に入れる
    (2) プレビューできるように、何かショートカットを割り当てておく

--- inits/30_text-adjust.el
+++ inits/30_text-adjust.el
@@ -6,3 +6,13 @@
 ;; taiyaki.org が落ちているので、以下から取得できる
 ;; http://d.hatena.ne.jp/rubikitch/comment/20090220/text_adjust
 (require 'text-adjust)
+
+
+;; howm のメモを書いている時と、プレインテキストを書いている時は、
+;; 半角英数字の前後に半角空白を入れて読み易くする。
+(defun text-adjust-space-before-save-if-needed ()
+  (when (memq major-mode
+              '(howm-mode text-mode))
+    (text-adjust-space-buffer)))
+(defalias 'spacer 'text-adjust-space-buffer)
+(add-hook 'before-save-hook 'text-adjust-space-before-save-if-needed)
