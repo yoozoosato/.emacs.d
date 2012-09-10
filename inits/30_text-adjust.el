e238b8a2603963e6e91c9c7450f5b2ee73391dbc
SATO Yozo <Yozo.Sato@gamepot.co.jp>
2012-09-10 15:26:33 +0900

    検索していたらヒットした。
    るびきちさん、ありがとう。

--- inits/30_text-adjust.el
+++ inits/30_text-adjust.el
@@ -3,5 +3,6 @@
 ;;
 ;; (auto-install-from-url "http://taiyaki.org/elisp/mell/src/mell.el")
 ;; (auto-install-from-url "http://taiyaki.org/elisp/text-adjust/src/text-adjust.el")
-
+;; taiyaki.org が落ちているので、以下から取得できる
+;; http://d.hatena.ne.jp/rubikitch/comment/20090220/text_adjust
 (require 'text-adjust)
