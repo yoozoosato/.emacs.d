9b4671fd111dadff4bd3a4102c0a9280383e6da4
SATO Yozo <yoozoosato@gmail.com>
2013-04-01 16:36:44 +0900

    I don't use these modes on Emacs.

--- inits/00_keybind.el
+++ inits/00_keybind.el
@@ -38,14 +38,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "M-_") 'auto-complete)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; org-mode
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-
 ;; sequential-command-config
 ;; (auto-install-from-emacswiki "sequential-command.el")
 ;; (auto-install-from-emacswiki "sequential-command-config.el")
