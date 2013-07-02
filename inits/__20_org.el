af9892a18e82363ff2d452c5a8e67d8281b106c2
SATO Yozo <yoozoosato@gmail.com>
2013-07-02 10:20:32 +0900

    org-mode is too rich and complicated.

--- inits/__20_org.el
+++
@@ -1,6 +0,0 @@
-;; org-mode
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(setq org-use-fast-todo-selection t)
-(setq org-todo-keywords
-	  '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")))
