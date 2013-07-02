35308031ec093c10d3207e9e3529fdcccd73b3f6
SATO Yozo <yoozoosato@gmail.com>
2013-07-02 10:23:36 +0900

    Using web browser or dictionary is much faster.

--- inits/__30_translator.el
+++
@@ -1,8 +0,0 @@
-;;
-;; (auto-install-from-emacswiki "text-translator-vars.el")
-;; (auto-install-from-emacswiki "text-translator.el")
-;; (auto-install-from-emacswiki "text-translator-load.el")
-(require 'text-translator)
-(setq text-translator-auto-selection-func
-      'text-translator-translate-by-auto-selection-enja)
-
