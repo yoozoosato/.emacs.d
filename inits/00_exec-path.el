4c5435eb0d803e35328f41c04d8e8adbe3db2c22
SATO Yozo <yoozoosato@gmail.com>
2013-11-22 13:41:40 +0900

    nrepl-jack-in が動くようになった。
    細かい設定は後で足していく。

--- inits/00_exec-path.el
+++ inits/00_exec-path.el
@@ -1,27 +1,12 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; port install path
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; clock plugin in e2wm uses wget to get pictures from bijin-tokei
-;; 'wget' is installed using port so that required to set exec-path
-(cond
- ((string-match "apple-darwin" system-configuration)
-  (setq exec-path (cons "/opt/local/bin" exec-path))
-  )
-)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; for migemo
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(cond
- ((string-match "apple-darwin" system-configuration)
-  (setq exec-path (cons "/usr/local/bin" exec-path))
-  )
-)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; mew bin files.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq exec-path (cons "~/.emacs.d/lisp/mew/bin" exec-path))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Retrieve PATH settings from default shell settings.
+;; Thanks to http://qiita.com/catatsuy/items/3dda714f4c60c435bb25
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(exec-path-from-shell-initialize)
 
