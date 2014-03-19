51a35ee853717f0e04bcfcd76be6884068383e0a
SATO Yozo <yoozoosato@gmail.com>
2014-03-19 15:49:05 +0900

    when editing json, js2 mode will be enabled automatically.

--- inits/40_js2.el
+++ inits/40_js2.el
@@ -3,6 +3,7 @@
 ;; (auto-install-from-url "http://js2-mode.googlecode.com/files/js2-mode.el")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 (add-hook 'js2-mode-hook
           '(lambda ()
              (set-fill-column 80)
