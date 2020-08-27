03ee124483521ad9499fc04cf297885b8c8f1999
SATO Yozo <yoozoosato@gmail.com>
2020-08-27 17:05:06 +0900

    coging-system とかいじっているあたりが悪さしていたようなので削除

--- inits/50_magit.el
+++ inits/50_magit.el
@@ -1,9 +1,9 @@
 ;; magit
 (require 'magit)
 
-(add-hook 'server-visit-hook
-          (function (lambda ()
-                      (if (string-match "COMMIT_EDITMSG" buffer-file-name)
-                          (set-buffer-file-coding-system 'utf-8)))))
-(add-hook 'magit-mode-hook
-		  (set-buffer-file-coding-system 'utf-8))
+;; (add-hook 'server-visit-hook
+;;           (function (lambda ()
+;;                       (if (string-match "COMMIT_EDITMSG" buffer-file-name)
+;;                           (set-buffer-file-coding-system 'utf-8)))))
+;; (add-hook 'magit-mode-hook
+;; 		  (set-buffer-file-coding-system 'utf-8))
