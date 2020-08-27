aa1880e7ff02fc4adb715a83fd86e9015b49d69b
SATO Yozo <yoozoosato@gmail.com>
2020-08-27 17:16:55 +0900

    さすがにもう w3m は使[わ|え]ないので削除

--- inits/30_w3m.el
+++
@@ -1,9 +0,0 @@
-;;
-;; w3m can be installed using ELPA
-;;
-(setq browse-url-browser-function 'w3m-browse-url)
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-;; optional keyboard short-cut
-(global-set-key "\C-xm" 'browse-url-at-point)
-
-(setq w3m-use-cookies t)
