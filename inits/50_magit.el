45a124f4e22583fa33f180df4e563da60983ac41
SATO Yozo <yoozoosato@gmail.com>
2020-08-27 15:17:37 +0900

    エラーが出なくなった。 Git 操作もこれでできそう。

--- inits/50_magit.el
+++ inits/50_magit.el
@@ -1,12 +1,9 @@
 ;; magit
 (require 'magit)
 
-;; (add-hook 'server-visit-hook
-;;           (function (lambda ()
-;;                       (if (string-match "COMMIT_EDITMSG" buffer-file-name)
-;;                           (set-buffer-file-coding-system 'utf-8)))))
-;; (add-hook 'magit-mode-hook
-;; 		  (set-buffer-file-coding-system 'utf-8))
-
-
-;; (setq magit-last-seen-setup-instructions "1.4.0")
+(add-hook 'server-visit-hook
+          (function (lambda ()
+                      (if (string-match "COMMIT_EDITMSG" buffer-file-name)
+                          (set-buffer-file-coding-system 'utf-8)))))
+(add-hook 'magit-mode-hook
+		  (set-buffer-file-coding-system 'utf-8))
